12008ad802c527fc87c73207bfbbfe2def83bf5d
Dave Moreno <mordavidj@gmail.com>
2023-03-12 22:38:09 -0600

    Add files from extracted python package 0.5.1

--- client/python/setup.py
+++ client/python/setup.py
@@ -18,7 +18,7 @@ install_requires = ['protobuf >= 3.6']
 
 setup(
     name='krpc',
-    version=open(os.path.join(dirpath, 'VERSION.txt')).read(),
+    version=re.search(r'\'(.+)\'', open(os.path.join(dirpath, 'krpc/version.py')).read()).group(1),
     author='djungelorm',
     author_email='djungelorm@users.noreply.github.com',
     packages=['krpc', 'krpc.schema', 'krpc.test', 'krpc.test.schema'],
@@ -26,6 +26,7 @@ setup(
     license='GNU LGPL v3',
     description='Client library for kRPC, a Remote Procedure Call server for Kerbal Space Program',
     long_description=open(os.path.join(dirpath, 'README.txt')).read(),
+    python_requires='>=3.4',
     install_requires=install_requires,
     test_suite='krpc.test',
     classifiers=[
@@ -39,5 +40,6 @@ setup(
         'Topic :: Communications',
         'Topic :: Games/Entertainment :: Simulation',
         'Topic :: Internet'
-    ]
+    ],
+    package_data = {"krpc": ["py.typed","krpc.pyi","spacecenter.pyi","drawing.pyi","infernalrobotics.pyi","kerbalalarmclock.pyi","remotetech.pyi","ui.pyi","lidar.pyi","dockingcamera.pyi"]},
 )
